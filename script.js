88aadc349e0c33946da50469000d4cc31e705353
eazzie <eazzie960904@gmail.com>
2025-04-13 16:44:22 +0900

    250413

--- script.js
+++ script.js
@@ -13,7 +13,3 @@ const dialog = document.querySelector("dialog");
 button.addEventListener("click", () => {
   dialog.showModal();
 });
-
-dialog.addEventListener("close", () => {
-  console.log(dialog.returnValue);
-});
