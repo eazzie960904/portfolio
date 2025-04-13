825762de3464ab903917add7d412d78976f59da0
eazzie <eazzie960904@gmail.com>
2025-04-13 15:58:52 +0900

    250413

---
+++ script.js
@@ -0,0 +1,19 @@
+const footerInput = document.querySelector(".footerInput");
+const footerButton = document.querySelector(".footerButton");
+
+footerButton.addEventListener("click", footerButtonClick);
+
+function footerButtonClick() {
+  footerInput.value = "";
+}
+
+const button = document.querySelector("button");
+const dialog = document.querySelector("dialog");
+
+button.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+dialog.addEventListener("close", () => {
+  console.log(dialog.returnValue);
+});
